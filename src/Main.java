9428abe737805a15d0a118be73c506b18cdda278
Gabriel <bristotgl@gmail.com>
2023-03-13 04:01:15 -0300

    creates server socket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            final ServerSocket server = new ServerSocket(8080);
+            System.out.println("Listening for connection on port 8080 ...");
+            while (true) {}
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

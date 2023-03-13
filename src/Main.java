3a4610ba36d9c23507df4acc0ed9a8ba5f2b52a0
Gabriel <bristotgl@gmail.com>
2023-03-13 04:19:35 -0300

    adds reading of sockets contents

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,7 +10,18 @@ public class Main {
         try {
             final ServerSocket server = new ServerSocket(8080);
             System.out.println("Listening for connection on port 8080 ...");
-            while (true) {}
+
+            while (true) {
+                final Socket serverClient = server.accept();
+                final InputStreamReader isr = new InputStreamReader(serverClient.getInputStream());
+                final BufferedReader reader = new BufferedReader(isr);
+
+                String line = reader.readLine();
+                while (!line.isEmpty()) {
+                    System.out.println(line);
+                    line = reader.readLine();
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
